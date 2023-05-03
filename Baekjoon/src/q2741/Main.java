bbefe6b8e755df4a02708844762d39918cb77031
Class01 <Class01@DESKTOP-AHAB51C>
2023-05-03 14:59:43 +0900

    2741

---
+++ Baekjoon/src/q2741/Main.java
@@ -0,0 +1,16 @@
+package q2741;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N1 = sc.nextInt();
+		for (int i=1;i<=N1;i++) {
+			System.out.println(i);
+		}
+
+		sc.close();
+	}
+}
