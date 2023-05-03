ed8016ed4917dff7131d15d044954d63ac2863e5
Class01 <Class01@DESKTOP-AHAB51C>
2023-05-03 14:19:17 +0900

    18108

---
+++ Baekjoon/src/q18108/Main.java
@@ -0,0 +1,14 @@
+package q18108;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int year = sc.nextInt();
+		sc.close();
+		System.out.print(year-543);
+	}
+}
