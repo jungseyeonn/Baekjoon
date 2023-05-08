706a5294f2456a22fe5cea5cf7c9f984bf608ff2
Class01 <Class01@DESKTOP-AHAB51C>
2023-05-08 15:49:12 +0900

    2438 이중for문추가

--- Baekjoon/src/q2438/Main.java
+++ Baekjoon/src/q2438/Main.java
@@ -5,13 +5,22 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		String star = "*";
-		int n = sc.nextInt();
+
+//		String star = "*";
+//		int n = sc.nextInt();
+//		for (int i = 0; i < n; i++) {
+//			System.out.println(star);
+//			star = star + "*";
+//		}
+
+		// 이중for문 쓰기
+		int num = sc.nextInt();
 		sc.close();
-		for (int i=0;i<n;i++) {
-			System.out.println(star);
-			star = star+"*";
+		for (int i = 0; i < num; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
 		}
 	}
 }
