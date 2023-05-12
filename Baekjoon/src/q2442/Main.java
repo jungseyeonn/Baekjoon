e6bb567f5574e48580eaaa5db8e3242b76d03ba4
Class01 <Class01@DESKTOP-AHAB51C>
2023-05-12 16:32:42 +0900

    2442

---
+++ Baekjoon/src/q2442/Main.java
@@ -0,0 +1,21 @@
+package q2442;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		sc.close();
+
+		for (int i = 1; i <= num; i++) {
+			for (int j = i; j <= num-1; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < 2*i-1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
