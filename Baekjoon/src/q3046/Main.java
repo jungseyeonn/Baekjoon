7e56543ac2a2728d82e89cadb7fb329b89caab0d
Class01 <Class01@DESKTOP-AHAB51C>
2023-06-01 09:58:26 +0900

    3046

---
+++ Baekjoon/src/q3046/Main.java
@@ -0,0 +1,16 @@
+package q3046;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int R1 = sc.nextInt();
+		int S = sc.nextInt();
+		sc.close();
+		
+		int R2 = (2*S)-R1;
+		S = (R2+R1)/2;
+		System.out.println(R2);
+	}
+}
